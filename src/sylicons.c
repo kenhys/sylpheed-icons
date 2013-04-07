0d3cfcfc012e4206775fe534649fcbe6e07894f2
HAYASHI Kentaro <kenhys@gmail.com>
2013-04-07 16:17:44 +0900

    fix mixed declarations and codes

--- src/sylicons.c
+++ src/sylicons.c
@@ -398,6 +398,9 @@ static gchar* g_copyright = N_("SylIcons is distributed under GPL license.\n"
 
 static GtkWidget *create_config_main_page(GtkWidget *notebook, GKeyFile *pkey)
 {
+  GtkWidget *vbox;
+  GtkWidget *label;
+
   debug_print("create_config_main_page\n");
   if (notebook == NULL){
     return NULL;
@@ -405,10 +408,10 @@ static GtkWidget *create_config_main_page(GtkWidget *notebook, GKeyFile *pkey)
   /* startup */
   if (pkey!=NULL){
   }
-  GtkWidget *vbox = gtk_vbox_new(FALSE, 6);
+  vbox = gtk_vbox_new(FALSE, 6);
 
-  GtkWidget *general_lbl = gtk_label_new(_("General"));
-  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox, general_lbl);
+  label = gtk_label_new(_("General"));
+  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox, label);
   gtk_widget_show_all(notebook);
 
   return vbox;
@@ -417,33 +420,40 @@ static GtkWidget *create_config_main_page(GtkWidget *notebook, GKeyFile *pkey)
 /* about, copyright tab */
 static GtkWidget *create_config_about_page(GtkWidget *notebook, GKeyFile *pkey)
 {
+  GtkWidget *hbox, *vbox;
+  GtkWidget *misc;
+  GtkWidget *scrolled;
+  GtkWidget *label;
+  GtkTextBuffer *tbuffer;
+  GtkWidget *tview;
+  
   debug_print("create_config_about_page\n");
   if (notebook == NULL){
     return NULL;
   }
-  GtkWidget *hbox = gtk_hbox_new(TRUE, 6);
-  GtkWidget *vbox = gtk_vbox_new(FALSE, 6);
+  hbox = gtk_hbox_new(TRUE, 6);
+  vbox = gtk_vbox_new(FALSE, 6);
   gtk_box_pack_start(GTK_BOX(hbox), vbox, TRUE, TRUE, 6);
 
-  GtkWidget *misc = gtk_label_new("SylIcons");
+  misc = gtk_label_new("SylIcons");
   gtk_box_pack_start(GTK_BOX(vbox), misc, FALSE, TRUE, 6);
 
   misc = gtk_label_new(PLUGIN_DESC);
   gtk_box_pack_start(GTK_BOX(vbox), misc, FALSE, TRUE, 6);
 
   /* copyright */
-  GtkWidget *scrolled = gtk_scrolled_window_new(NULL, NULL);
+  scrolled = gtk_scrolled_window_new(NULL, NULL);
 
-  GtkTextBuffer *tbuffer = gtk_text_buffer_new(NULL);
+  tbuffer = gtk_text_buffer_new(NULL);
   gtk_text_buffer_set_text(tbuffer, _(g_copyright), strlen(g_copyright));
-  GtkWidget *tview = gtk_text_view_new_with_buffer(tbuffer);
+  tview = gtk_text_view_new_with_buffer(tbuffer);
   gtk_text_view_set_editable(GTK_TEXT_VIEW(tview), FALSE);
   gtk_container_add(GTK_CONTAINER(scrolled), tview);
 
   gtk_box_pack_start(GTK_BOX(vbox), scrolled, TRUE, TRUE, 6);
 
-  GtkWidget *general_lbl = gtk_label_new(_("About"));
-  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), hbox, general_lbl);
+  label = gtk_label_new(_("About"));
+  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), hbox, label);
   gtk_widget_show_all(notebook);
   return NULL;
 }
